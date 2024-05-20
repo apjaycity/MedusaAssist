1c58ffc9f7433ed9522f240da5483dc3c6e495ba
apjaycity <yomader073@gmail.com>
2024-05-20 19:08:05 -0400

    Auto commit by Orobus

--- Utils/APJ_OFWP.ps1
+++ Utils/APJ_OFWP.ps1
@@ -1,13 +1,19 @@
 $params = $args -split ","
 $cmd = $params[0]
 $arg = $params[1]
+if( $null -eq $params[2]) {
+    $sub = ""
+} else {
+    $arg = $arg + $params[2]
+}
+
 Write-Output "Command: $cmd"
 Write-Output "Argument: $arg"
 
 try {
     $ps = New-Object System.Diagnostics.Process
     $ps.StartInfo.FileName = $cmd
-    $ps.StartInfo.Arguments = $arg
+    $ps.StartInfo.Arguments = $arg+" "+$sub
     $ps.StartInfo.RedirectStandardOutput = $true
     $ps.StartInfo.RedirectStandardError = $true
     $ps.StartInfo.UseShellExecute = $false
--- serviceScripts/gitService.ps1
+++ serviceScripts/gitService.ps1
@@ -2,5 +2,6 @@
 git add -A "D:\orobus\medusa\*"
 git commit -a -m "Auto commit by Orobus"
 git push --force
+Start-Sleep -Seconds 60 * 10
 
 
